972c5dffe0e640be169340955cc50626f069addf
Ian Waldrop <waldropij@gmail.com>
2016-04-07 11:03:58 -0700

    adding tests

---
+++ Assets/Konglomerate/UFind/Editor/Tests/IntExtensionTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace UFind
+{
+	public class IntExtensionTests
+	{
+		const int LOWER_BOUND = 0;
+		const int UPPER_BOUND = 10;
+
+		[Test(Description = "Numbers smaller than the range should be wrapped to the upper bound")]
+		public void WrapSmallNumber_IsEqualToUpperBound()
+		{
+			var testNumber = -10;
+			var result = testNumber.Wrap(LOWER_BOUND, UPPER_BOUND);
+			Assert.AreEqual(result, UPPER_BOUND);
+		}
+
+		[Test(Description = "Numbers larger than the range should be wrapped to the lower bound")]
+		public void WrapLargeNumber_IsEqualToLowerBound()
+		{
+			var testNumber = 20;
+			var result = testNumber.Wrap(LOWER_BOUND, UPPER_BOUND);
+			Assert.AreEqual(result, LOWER_BOUND);
+		}
+
+		[Test(Description = "Numbers which fall within the lower and upper bounds should remain unchanged")]
+		public void ContainedNumber_IsEqualToStartingValue()
+		{
+			const int INITIAL_VALUE = 5;
+			var testNumber = INITIAL_VALUE;
+			var result = testNumber.Wrap(LOWER_BOUND, UPPER_BOUND);
+			Assert.AreEqual(result, INITIAL_VALUE);
+		}
+	}
+}
---
+++ Assets/Konglomerate/UFind/Editor/Tests/StringExtensionTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace UFind
+{
+	public class StringExtensionTests
+	{
+		const string CONTROL = "The quick brown fox jumped over the lazy dog";
+
+		[Test]
+		public void LowerCaseInsensitiveMatch_IsTrue()
+		{
+			var result = CONTROL.CaseInsensitiveContains("the");
+			Assert.IsTrue(result);
+		}
+
+		[Test]
+		public void UpperCaseInsensitiveContains_IsTrue()
+		{
+			var result = CONTROL.CaseInsensitiveContains("THE");
+			Assert.IsTrue(result);
+		}
+
+		[Test(Description = "Compares the control string to a string which is not contained to assure there are no false positives")]
+		public void LowerCaseInsensitiveContains_IsFalse()
+		{
+			var result = CONTROL.CaseInsensitiveContains("you shall not pass");
+			Assert.IsFalse(result);
+		}
+
+		[Test(Description = "Compares the control string to a string which is not contained to assure there are no false positives")]
+		public void UpperCaseInsensitiveContains_IsFalse()
+		{
+			var result = CONTROL.CaseInsensitiveContains("YOU SHALL NOT PASS");
+			Assert.IsFalse(result);
+		}
+	}
+}
