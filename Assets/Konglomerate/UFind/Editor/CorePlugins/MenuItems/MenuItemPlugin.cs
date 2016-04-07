593233637432f058b35b81a859e7d3136908aed0
Ian Waldrop <waldropij@gmail.com>
2016-04-07 11:04:31 -0700

    initial menu item plugin, not really working

---
+++ Assets/Konglomerate/UFind/Editor/CorePlugins/MenuItems/MenuItemPlugin.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using System.Collections.Generic;
+using System;
+using UnityEditor;
+
+namespace UFind
+{
+	public class MenuItemPlugin : UFPlugin
+	{
+		#region implemented abstract members of UFPlugin
+		protected override IEnumerable<IFinderResult> GetObjectResults(IFinderContext context)
+		{
+			var list = new List<IFinderResult>();
+
+			var types = Assembly.GetCallingAssembly().GetTypes();
+			foreach (var type in types)
+			{
+				foreach (var method in type.GetMethods(BindingFlags.Static | BindingFlags.Public))
+				{
+					var item = Attribute.GetCustomAttribute(method, typeof(MenuItem)) as MenuItem;
+					if (item != null)
+					{
+						if (item.menuItem.Contains("Window/UFind"))
+						{
+							continue;
+						}
+
+						if (context.GetIsMatchForTerm(item.menuItem))
+						{
+							list.Add(new MenuItemResult(item, method));
+						}
+					}
+				}
+			}
+
+			return list;
+		}
+
+		public override string Name { get { return "Menu Items"; } }
+		#endregion
+	}
+}
---
+++ Assets/Konglomerate/UFind/Editor/CorePlugins/MenuItems/MenuItemResult.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Reflection;
+using UnityEditor;
+
+namespace UFind
+{
+	public class MenuItemResult : UFResult
+	{
+		public readonly MethodInfo method;
+		public readonly MenuItem item;
+
+		public MenuItemResult(MenuItem item, MethodInfo method)
+		{
+			this.method = method;
+			this.item = item;
+		}
+
+		#region implemented abstract members of UFResult
+		public override GUIContent Description { get { return null; } }
+
+		public override string Title { get { return item.menuItem; } }
+
+		public override void Execute(IFinderContext context)
+		{
+			method.Invoke(null, null);
+		}
+
+		protected override Object UnityObject
+		{
+			get { return new TextAsset(); }
+		}
+		#endregion
+	}
+}
